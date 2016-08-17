790c6437f51d925914a00832c25c35db52bac456
Ella Torregrosa <ellatorregrosapv@gmail.com>
2016-08-17 14:33:56 -0400

    to help shormie

--- backend.py
+++ backend.py
@@ -2,7 +2,39 @@ import pygame
 
 import constantVars
 from rPlayer import player
+import pygame
+ 
 pygame.init()
+ 
+ 
+class MenuItem(pygame.font.Font):
+    def __init__(self, text, font=None, font_size=30, font_color=(255, 255, 255), (pos_x, pos_y)=(0, 0)):
+        pygame.font.Font.__init__(self, font, font_size)
+        self.text = text
+        self.font_size = font_size
+        self.font_color = font_color
+        self.label = self.render(self.text, 1, self.font_color)
+        self.width = self.label.get_rect().width
+        self.height = self.label.get_rect().height
+        self.dimensions = (self.width, self.height)
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.position = pos_x, pos_y
+ 
+    def set_position(self, x, y):
+        self.position = (x, y)
+        self.pos_x = x
+        self.pos_y = y
+ 
+    def set_font_color(self, rgb_tuple):
+        self.font_color = rgb_tuple
+        self.label = self.render(self.text, 1, self.font_color)
+ 
+    def is_mouse_selection(self, (posx, posy)):
+        if (posx >= self.pos_x and posx <= self.pos_x + self.width) and \
+            (posy >= self.pos_y and posy <= self.pos_y + self.height):
+                return True
+        return False
  
  
 class GameMenu():
@@ -15,23 +47,19 @@ class GameMenu():
         self.bg_color = bg_color
         self.clock = pygame.time.Clock()
  
-        self.items = items
-        self.font = pygame.font.SysFont(font, font_size)
-        self.font_color = font_color
- 
         self.items = []
         for index, item in enumerate(items):
-            label = self.font.render(item, 1, font_color)
+            menu_item = MenuItem(item)#, '/home/nebelhom/.fonts/SHOWG.TTF')
  
-            width = label.get_rect().width
-            height = label.get_rect().height
- 
-            posx = (self.scr_width / 2) - (width / 2)
             # t_h: total height of text block
-            t_h = len(items) * height
-            posy = (self.scr_height / 2) - (t_h / 2) + (index * height)
+            t_h = len(items) * menu_item.height
+            pos_x = (self.scr_width / 2) - (menu_item.width / 2)
+            # This line includes a bug fix by Ariel (Thanks!)
+            # Please check the comments section for an explanation
+            pos_y = (self.scr_height / 2) - (t_h / 2) + ((index * 2) + index * menu_item.height)
  
-            self.items.append([item, label, (width, height), (posx, posy)])
+            menu_item.set_position(pos_x, pos_y)
+            self.items.append(menu_item)
  
     def run(self):
         mainloop = True
@@ -46,8 +74,14 @@ class GameMenu():
             # Redraw the background
             self.screen.fill(self.bg_color)
  
-            for name, label, (width, height), (posx, posy) in self.items:
-                self.screen.blit(label, (posx, posy))
+            for item in self.items:
+                if item.is_mouse_selection(pygame.mouse.get_pos()):
+                    item.set_font_color((255, 0, 0))
+                    item.set_italic(True)
+                else:
+                    item.set_font_color((255, 255, 255))
+                    item.set_italic(False)
+                self.screen.blit(item.label, item.position)
  
             pygame.display.flip()
  
