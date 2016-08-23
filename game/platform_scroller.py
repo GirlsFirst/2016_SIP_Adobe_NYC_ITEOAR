fb433b36786c81bfc42e9c4ca69022fd8097400c
Ella Torregrosa <ellatorregrosapv@gmail.com>
2016-08-23 10:48:37 -0400

    work pls

--- game/platform_scroller.py
+++ game/platform_scroller.py
@@ -18,304 +18,237 @@ plauer = Player()
 
 # Screen Menu Stuff
 def text_objects(text, font):
-	textSurface = font.render(text, True, constants.WHITE)
-	return textSurface, textSurface.get_rect()
+    textSurface = font.render(text, True, constants.WHITE)
+    return textSurface, textSurface.get_rect()
 
 def button(msg,x,y,w,h,color,action=None):
 
-	mouse = pygame.mouse.get_pos()
-	click = pygame.mouse.get_pressed()
-	# print(click)
-	if x+w > mouse[0] > x and y+h > mouse[1] > y:
-		pygame.draw.rect(screen, color,(x,y,w,h), 5)
-		if click[0] == 1 and action != None:
-			if action == "play":
-				main()
-			elif action == "quit":
-				pygame.quit()
-				quit()
-			elif action == "instructions":
-				game_instructions()
-	else:
-		pygame.draw.rect(screen, color,(x,y,w,h), 3)
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    # print(click)
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(screen, color,(x,y,w,h), 5)
+        if click[0] == 1 and action != None:
+            if action == "play":
+                main()
+            elif action == "quit":
+                pygame.quit()
+                quit()
+            elif action == "instructions":
+                game_instructions()
+    else:
+        pygame.draw.rect(screen, color,(x,y,w,h), 3)
 
-	smallText = pygame.font.Font("freesansbold.ttf",20)
-	textSurf, textRect = text_objects(msg, smallText)
-	textRect.center = ( (x+(w/2)), (y+(h/2)) )
-	screen.blit(textSurf, textRect)
+    smallText = pygame.font.Font("freesansbold.ttf",20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    screen.blit(textSurf, textRect)
 
 def game_intro():
+    intro = True
+    bg = pygame.image.load("start.gif")
+
+    while intro:
+        for event in pygame.event.get():
+            #print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        screen.fill(constants.WHITE)
+        largeText = pygame.font.Font('freesansbold.ttf', 50)
+        TextSurf, TextRect = text_objects("In the Eyes of a Refugee", largeText)
+        TextRect.center = ((constants.SCREEN_WIDTH/2),(constants.SCREEN_HEIGHT/2))
+        screen.blit(TextSurf, TextRect)
 
 
-	intro = True
-	bg = pygame.image.load("start.gif")
+        mouse = pygame.mouse.get_pos()
 
-	while intro:
-		for event in pygame.event.get():
-			#print(event)
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				quit()
-				
-		screen.fill(constants.WHITE)
-		largeText = pygame.font.Font('freesansbold.ttf', 50)
-		TextSurf, TextRect = text_objects("In the Eyes of a Refugee", largeText)
-		TextRect.center = ((constants.SCREEN_WIDTH/2),(constants.SCREEN_HEIGHT/2))
-		screen.blit(TextSurf, TextRect)
+        # print(mouse)
+                
+        # screen.fill((0,0,0))
+        screen.blit(bg, (0,0))
+        largeText = pygame.font.Font('freesansbold.ttf',50)
+        TextSurf, TextRect = text_objects("In the Eyes of a Refugee", largeText)
+        TextRect.center = ((constants.SCREEN_WIDTH/2),(constants.SCREEN_HEIGHT/2))
+        screen.blit(TextSurf, TextRect)
 
+        button("START", 150,450,100,50,constants.WHITE,"play")
+        button("INSTRUCTIONS", 300, 450, 200, 50, constants.WHITE, "instructions")
+        button("QUIT", 550,450,100,50,constants.WHITE,"quit")
+        mouse = pygame.mouse.get_pos()
 
-		mouse = pygame.mouse.get_pos()
+        # print(mouse)
 
-		# print(mouse)
-				
-		# screen.fill((0,0,0))
-		screen.blit(bg, (0,0))
-		largeText = pygame.font.Font('freesansbold.ttf',50)
-		TextSurf, TextRect = text_objects("In the Eyes of a Refugee", largeText)
-		TextRect.center = ((constants.SCREEN_WIDTH/2),(constants.SCREEN_HEIGHT/2))
-		screen.blit(TextSurf, TextRect)
+        clock = pygame.time.Clock()
 
-		button("START", 150,450,100,50,constants.WHITE,"play")
-		button("INSTRUCTIONS", 300, 450, 200, 50, constants.WHITE, "instructions")
-		button("QUIT", 550,450,100,50,constants.WHITE,"quit")
-		mouse = pygame.mouse.get_pos()
-
-		# print(mouse)
-
-		clock = pygame.time.Clock()
-
-		pygame.display.update()
-		clock.tick(15) 
+        pygame.display.update()
+        clock.tick(15) 
 
 def game_instructions():
-	instructions = True
-	bg = pygame.image.load("waves.png")
+    instructions = True
+    bg = pygame.image.load("waves.png")
 
-	while instructions:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				quit()
-			
-		screen.fill(constants.WHITE)
-		screen.blit(bg, (0,0))
+    while instructions:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            
+        screen.fill(constants.WHITE)
+        screen.blit(bg, (0,0))
 
-		largeText = pygame.font.Font('freesansbold.ttf',50)
-		TextSurf, TextRect = text_objects("Instructions", largeText)
-		# Text is centered at the top middle
-		TextRect.center = ((constants.SCREEN_WIDTH/2),(constants.SCREEN_HEIGHT/5))
-		screen.blit(TextSurf, TextRect)
+        largeText = pygame.font.Font('freesansbold.ttf',50)
+        TextSurf, TextRect = text_objects("Instructions", largeText)
+        # Text is centered at the top middle
+        TextRect.center = ((constants.SCREEN_WIDTH/2),(constants.SCREEN_HEIGHT/5))
+        screen.blit(TextSurf, TextRect)
 
-		button("START", 150,450,100,50,constants.WHITE,"play")
-		button("QUIT", 550,450,100,50,constants.WHITE,"quit")
-		mouse = pygame.mouse.get_pos()
+        button("START", 150,450,100,50,constants.WHITE,"play")
+        button("QUIT", 550,450,100,50,constants.WHITE,"quit")
+        mouse = pygame.mouse.get_pos()
 
-		clock = pygame.time.Clock()
-		pygame.display.update()
-		clock.tick(15) 
+        clock = pygame.time.Clock()
+        pygame.display.update()
+        clock.tick(15) 
 
-	mouse = pygame.mouse.get_pos()
-	click = pygame.mouse.get_pressed()
-	# print(click)
-	if x+w > mouse[0] > x and y+h > mouse[1] > y:
-		pygame.draw.rect(screen, color,(x,y,w,h), 5)
-		if click[0] == 1 and action != None:
-			if action == "start":
-				game_instructions()
-			elif action == "quit":
-				pygame.quit()
-				quit()
-			elif action == "play":
-				main()
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    # print(click)
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(screen, color,(x,y,w,h), 5)
+        if click[0] == 1 and action != None:
+            if action == "start":
+                game_instructions()
+            elif action == "quit":
+                pygame.quit()
+                quit()
+            elif action == "play":
+                main()
 
-	else:
-		pygame.draw.rect(screen, color,(x,y,w,h), 3)
-	# pygame.font.init()
+    else:
+        pygame.draw.rect(screen, color,(x,y,w,h), 3)
+    # pygame.font.init()
 
-	smallText = pygame.font.Font("freesansbold.ttf",20)
-	textSurf, textRect = text_objects(msg, smallText)
-	textRect.center = ( (x+(w/2)), (y+(h/2)) )
-	screen.blit(textSurf, textRect)
-
-# def game_intro():
-# 	intro = True
-# 	bg = pygame.image.load("start.gif")
-
-# 	while intro:
-# 		for event in pygame.event.get():
-# 			#print(event)
-# 			if event.type == pygame.QUIT:
-# 				pygame.quit()
-# 				quit()
-# 		screen.fill(constants.WHITE)
-# 		largeText = pygame.font.Font('freesansbold.ttf', 50)
-# 		TextSurf, TextRect = text_objects("In the Eyes of a Refugee", largeText)
-# 		TextRect.center = ((constants.SCREEN_WIDTH/2),(constants.SCREEN_HEIGHT/2))
-# 		screen.blit(TextSurf, TextRect)
-# 		mouse = pygame.mouse.get_pos()
-
-# 		screen.blit(bg, (0,0))
-# 		largeText = pygame.font.Font('freesansbold.ttf',50)
-# 		TextSurf, TextRect = text_objects("In the Eyes of a Refugee", largeText)
-# 		TextRect.center = ((constants.SCREEN_WIDTH/2),(constants.SCREEN_HEIGHT/2))
-# 		screen.blit(TextSurf, TextRect)
-
-# 		button("START", 150,450,100,50,constants.WHITE,"start")
-# 		button("QUIT", 550,450,100,50,constants.WHITE,"quit")
-# 		mouse = pygame.mouse.get_pos()
-
-# 		# print(mouse)
-# 		pygame.display.update()
-# 		clock = pygame.time.Clock()
-# 		clock.tick(15) 
-
-# def game_instructions():
-# 	instructions = True
-# 	bg = pygame.image.load("waves.png")
-# 	while instructions:
-# 		for event in pygame.event.get():
-# 			if event.type == pygame.QUIT:
-# 				pygame.quit()
-# 				quit()
-			
-# 		# screen.fill(constants.WHITE)
-# 		# largeText = pygame.font.Font('freesansbold.ttf', 50)
-# 		# TextSurf, TextRect = text_objects("Instructions", largeText)
-# 		# TextRect.center = ((constants.SCREEN_WIDTH/2),(constants.SCREEN_HEIGHT/2))
-# 		# screen.blit(TextSurf, TextRect)
-# 		# mouse = pygame.mouse.get_pos()
-
-# 		screen.blit(bg, (0,0))
-# 		largeText = pygame.font.Font('freesansbold.ttf',50)
-# 		TextSurf, TextRect = text_objects("Instructions", largeText)
-# 		TextRect.center = ((constants.SCREEN_WIDTH/2),(constants.SCREEN_HEIGHT/5))
-# 		screen.blit(TextSurf, TextRect)
-
-# 		button("PLAY", 150,450,100,50,constants.WHITE,"play")
-# 		button("QUIT", 550,450,100,50,constants.WHITE,"quit")
-# 		mouse = pygame.mouse.get_pos()
-
-# 		clock = pygame.time.Clock()
-# 		pygame.display.update()
-# 		clock.tick(15) 
-
+    smallText = pygame.font.Font("freesansbold.ttf",20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    screen.blit(textSurf, textRect)
 
 def main():
 
-	""" Main Program """
-	pygame.init()
+    """ Main Program """
+    pygame.init()
 
-	# Set the height and width of screen
-	size = [constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT]
-	screen = pygame.display.set_mode(size)
+    # Set the height and width of screen
+    size = [constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT]
+    screen = pygame.display.set_mode(size)
 
-	pygame.display.set_caption("Platformer with sprite sheets")
+    pygame.display.set_caption("Platformer with sprite sheets")
 
-	# Create player
-	player = Player()
+    # Create player
+    player = Player()
 
-	# Create all levels
+    # Create all levels
 
-	""" Main Program """
-	# Set the height and width of the screen
-	# Create the player
-	# Create all the levels
+    """ Main Program """
+    # Set the height and width of the screen
+    # Create the player
+    # Create all the levels
 
-	level_list = []
-	# levelNow = 1
-	level_list.append(levels.Level_01(player))
-	# levelNow = 2
-	level_list.append(levels.Level_02(player))
-	# levelNow = 3
-	level_list.append(levels.Level_03(player))
+    level_list = []
+    # levelNow = 1
+    level_list.append(levels.Level_01(player))
+    # levelNow = 2
+    level_list.append(levels.Level_02(player))
+    # levelNow = 3
+    level_list.append(levels.Level_03(player))
 
-	# Set the current level
-	current_level_no = 0
-	current_level = level_list[current_level_no]
+    # Set the current level
+    current_level_no = 0
+    current_level = level_list[current_level_no]
 
-	active_sprite_list = pygame.sprite.Group()
-	player.level = current_level
+    active_sprite_list = pygame.sprite.Group()
+    player.level = current_level
 
-	player.rect.x = 320
-	player.rect.y = constants.SCREEN_HEIGHT - player.rect.height
-	active_sprite_list.add(player)
+    player.rect.x = 320
+    player.rect.y = constants.SCREEN_HEIGHT - player.rect.height
+    active_sprite_list.add(player)
 
-	#Loop until the user clicks the close button
-	done = False
+    #Loop until the user clicks the close button
+    done = False
 
-	# Used for FPS
-	clock = pygame.time.Clock()
+    # Used for FPS
+    clock = pygame.time.Clock()
 
-	# -------- Main Program Loop -----------
-	while not done:
-		for event in pygame.event.get(): 
-			if event.type == pygame.QUIT: 
-				done = True 
+    # -------- Main Program Loop -----------
+    while not done:
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                done = True 
 
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_LEFT:
-					player.go_left()
-				if event.key == pygame.K_RIGHT:
-					player.go_right()
-				if event.key == pygame.K_UP:
-					player.jump()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player.go_left()
+                if event.key == pygame.K_RIGHT:
+                    player.go_right()
+                if event.key == pygame.K_UP:
+                    player.jump()
 
-			if event.type == pygame.KEYUP:
-				if event.key == pygame.K_LEFT and player.change_x < 0:
-					player.stop()
-				if event.key == pygame.K_RIGHT and player.change_x > 0:
-					player.stop()
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT and player.change_x < 0:
+                    player.stop()
+                if event.key == pygame.K_RIGHT and player.change_x > 0:
+                    player.stop()
 
-		# Update player
-		active_sprite_list.update()
+        # Update player
+        active_sprite_list.update()
 
-		# Update current level
-		current_level.update()
+        # Update current level
+        current_level.update()
 
-		# If the player nears right side, shift world left
-		if player.rect.x >= 500:
-			diff = player.rect.x - 500
-			player.rect.x = 500
-			current_level.shift_world(-diff)
+        # If the player nears right side, shift world left
+        if player.rect.x >= 500:
+            diff = player.rect.x - 500
+            player.rect.x = 500
+            current_level.shift_world(-diff)
 
-		# If the user gets nears left side, shift world right
-		if player.rect.x <= 120:
-			diff = 120 - player.rect.x
-			player.rect.x = 120
-			current_level.shift_world(diff)
+        # If the user gets nears left side, shift world right
+        if player.rect.x <= 120:
+            diff = 120 - player.rect.x
+            player.rect.x = 120
+            current_level.shift_world(diff)
 
-		# Update to next level when reach end of current level
-		current_position = player.rect.x + current_level.world_shift
-		if current_position < current_level.level_limit:
-			player.rect.x = 120
-			if current_level_no < len(level_list)-1:
-				current_level_no += 1
-				current_level = level_list[current_level_no]
-				player.level = current_level
+        # Update to next level when reach end of current level
+        current_position = player.rect.x + current_level.world_shift
+        if current_position < current_level.level_limit:
+            player.rect.x = 120
+            if current_level_no < len(level_list)-1:
+                current_level_no += 1
+                current_level = level_list[current_level_no]
+                player.level = current_level
 
-		# ALL CODE TO DRAW SHOULD GO BELOW THIS COMMENT
-		current_level.draw(screen)
-		active_sprite_list.draw(screen)
-		# collideSpriteList.draw(screen)
+        # ALL CODE TO DRAW SHOULD GO BELOW THIS COMMENT
+        current_level.draw(screen)
+        active_sprite_list.draw(screen)
+        # collideSpriteList.draw(screen)
 
-		# ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
+        # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
 
-		# Limit to 60 FPS
-		clock.tick(60)
+        # Limit to 60 FPS
+        clock.tick(60)
 
-		# Go ahead and update the screen with what we've drawn.
-		pygame.display.flip()
+        # Go ahead and update the screen with what we've drawn.
+        pygame.display.flip()
 
-	# Be IDLE friendly. If you forget this line, the program will 'hang'
-	# on exit.
-	pygame.quit()
+    # Be IDLE friendly. If you forget this line, the program will 'hang'
+    # on exit.
+    pygame.quit()
 
 if __name__ == "__main__":
 
-	game_intro()
-	game_instructions()
-	main()
-
-	""" Main Program """
+    game_intro()
+    game_instructions()
+    main()
 
 
